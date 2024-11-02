5a7313933a155d321a9903f4c1addb65c09bb1d6
Duke Norsworthy <duke.norsworthy@gmail.com>
2024-11-02 09:38:18 -0400

    initial

---
+++ src/Synopsis.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// export default function Synopsis({synopsis}) {
+//   return (
+//     <div>       
+//         <div style={{ marginTop: '20px' }}>
+//             <h2>Chart Synopsis</h2>
+//             <p>{synopsis}</p>
+//         </div>
+//     </div>
+//   )
+// }
